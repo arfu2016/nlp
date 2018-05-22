7b5f836a24db6b3aed059c17afceb3bca4b61bb2
deco <deco@cubee.com>
2018-05-22 18:45:54 +0800

    Update nlp_models

---
+++ nlp_models/random_data/uniform_distribution.py
@@ -0,0 +1,15 @@
+"""
+@Project   : DuReader
+@Module    : uniform_distribution.py
+@Author    : Deco [deco@cubee.com]
+@Created   : 5/22/18 6:37 PM
+@Desc      : 
+"""
+import random
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    print(random.uniform(0, 100))
+    print(np.random.uniform(0, 100, 2))
