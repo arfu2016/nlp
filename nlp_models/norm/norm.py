00885e0f00bfbe5774236e94602bea8c4cd27c55
deco <deco@cubee.com>
2018-08-30 18:34:27 +0800

    Update nlp_models

---
+++ nlp_models/norm/norm.py
@@ -0,0 +1,11 @@
+import math
+
+def __abs__(self):
+	return math.hypot(self.x, self.y)
+
+import numpy as np
+np.linalg.norm(st_vector)
+
+import tensorflow as tf
+sts_encode1 = tf.nn.l2_normalize(embed(sts_input1))
+
